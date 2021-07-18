6fded637b57a41ddf434f18ab5fe71934c4ae097
Artur Gręziak <alex4448@wp.pl>
2021-07-18 18:49:05 +0200

    commit

--- app/main-page.js
+++ app/main-page.js
@@ -1,18 +1,27 @@
-// function myPageWasLoaded(){
-//   console.log(`This method was fired`)
+
+const Physics = require ('nativescript-physics-js')
+
+exports.onLoaded = function(args){
+    const page = args.object
+    const container = page.getViewById('game')
+    const meta = page.getViewById('meta')
+}
+
+const wold = Physics()
+world.add(Physics.renderer('ns', {
+  container: container,
+  metaText: meta,
+  meta: true
+}))
+
+// setInterval(() => {
+//   wold.render()
+// }, 17);
+
+// import { createViewModel } from "./main-view-model";
+
+// export function onNavigatingTo(args) {
+//   const page = args.object;
+
+//   page.bindingContext = createViewModel();
 // }
-// exports.myPageWasLoaded = myPageWasLoaded;
-
-
-function myButton() {
-  alert("Hello Word!");
-}
-exports.myButton = myButton;
-
-import { createViewModel } from "./main-view-model";
-
-export function onNavigatingTo(args) {
-  const page = args.object;
-
-  page.bindingContext = createViewModel();
-}
